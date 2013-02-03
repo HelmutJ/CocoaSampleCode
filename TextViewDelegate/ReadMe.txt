65b9fa0b0d66c6fbf7efa0d94d4cc7e89ed9ff14
Darcy Liu <tearnon@live.com>
2013-02-03 12:21:26 +0800

    add TextViewDelegate

---
+++ TextViewDelegate/ReadMe.txt
@@ -0,0 +1,31 @@
+TextViewDelegate
+==============
+
+ABOUT:
+
+This application demonstrates the use of the text view's delegate to control selection 
+  and user input.  We use an unmodified text view, except that as its delegate we make
+  sure that whenever a return is entered, the text that has been typed up to that point
+  is colored red, and is recorded as committed, so that no further changes are allowed 
+  to it.
+  
+===========================================================================
+BUILD REQUIREMENTS:
+
+Xcode 3.2, Mac OS X 10.6 Snow Leopard or later
+
+===========================================================================
+RUNTIME REQUIREMENTS:
+
+Mac OS X 10.6 Snow Leopard or later
+
+===========================================================================
+CHANGES FROM PREVIOUS VERSIONS:
+
+Version 1.1
+- Project updated for Xcode 4.
+Version 1.0
+- First version.
+
+===========================================================================
+Copyright (C) 2002-2011 Apple Inc. All rights reserved.
---
+++ TextViewDelegate/TextViewDelegate/Controller.m
@@ -0,0 +1,77 @@
+/*
+     File: Controller.m 
+ Abstract: Purpose or function of this class goes here. 
+  Version: 1.1 
+  
+ Disclaimer: IMPORTANT:  This Apple software is supplied to you by Apple 
+ Inc. ("Apple") in consideration of your agreement to the following 
+ terms, and your use, installation, modification or redistribution of 
+ this Apple software constitutes acceptance of these terms.  If you do 
+ not agree with these terms, please do not use, install, modify or 
+ redistribute this Apple software. 
+  
+ In consideration of your agreement to abide by the following terms, and 
+ subject to these terms, Apple grants you a personal, non-exclusive 
+ license, under Apple's copyrights in this original Apple software (the 
+ "Apple Software"), to use, reproduce, modify and redistribute the Apple 
+ Software, with or without modifications, in source and/or binary forms; 
+ provided that if you redistribute the Apple Software in its entirety and 
+ without modifications, you must retain this notice and the following 
+ text and disclaimers in all such redistributions of the Apple Software. 
+ Neither the name, trademarks, service marks or logos of Apple Inc. may 
+ be used to endorse or promote products derived from the Apple Software 
+ without specific prior written permission from Apple.  Except as 
+ expressly stated in this notice, no other rights or licenses, express or 
+ implied, are granted by Apple herein, including but not limited to any 
+ patent rights that may be infringed by your derivative works or by other 
+ works in which the Apple Software may be incorporated. 
+  
+ The Apple Software is provided by Apple on an "AS IS" basis.  APPLE 
+ MAKES NO WARRANTIES, EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION 
+ THE IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS 
+ FOR A PARTICULAR PURPOSE, REGARDING THE APPLE SOFTWARE OR ITS USE AND 
+ OPERATION ALONE OR IN COMBINATION WITH YOUR PRODUCTS. 
+  
+ IN NO EVENT SHALL APPLE BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL 
+ OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE, REPRODUCTION, 
+ MODIFICATION AND/OR DISTRIBUTION OF THE APPLE SOFTWARE, HOWEVER CAUSED 
+ AND WHETHER UNDER THEORY OF CONTRACT, TORT (INCLUDING NEGLIGENCE), 
+ STRICT LIABILITY OR OTHERWISE, EVEN IF APPLE HAS BEEN ADVISED OF THE 
+ POSSIBILITY OF SUCH DAMAGE. 
+  
+ Copyright (C) 2011 Apple Inc. All Rights Reserved. 
+  
+ */
+
+#import "Controller.h"
+
+@implementation Controller
+
+- (BOOL)textView:(NSTextView *)textView shouldChangeTextInRange:(NSRange)affectedCharRange replacementString:(NSString *)replacementString {
+    // Allow changes only for uncommitted text
+    return affectedCharRange.location >= committedLength;
+}
+
+- (BOOL)textView:(NSTextView *)textView doCommandBySelector:(SEL)commandSelector {
+    BOOL retval = NO;
+    
+    // When return is entered, record and color the newly committed text
+    if (@selector(insertNewline:) == commandSelector) {
+        
+        NSUInteger textLength = [[textView string] length];
+        if (textLength > committedLength) {
+            [textView setSelectedRange:NSMakeRange(textLength, 0)];
+            [textView insertText:@"\n"];
+            [textView setTextColor:[NSColor redColor] range:NSMakeRange(committedLength, textLength - committedLength)];
+            textLength++;
+            committedLength = textLength;
+        }
+        retval = YES;
+    }
+    
+    return retval;
+}
+
+@end
